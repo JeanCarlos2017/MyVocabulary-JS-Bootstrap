be2a67162f9e8d65cad7374b30149254a433d30f
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-02 18:15:35 -0300

    exibir dados na tabela
    o dado JSON - vindo do LeitorArquivoXLSX - são mostrados na tabela da página principal

--- js/index.js
+++ js/index.js
@@ -8,10 +8,17 @@ fileSelector.addEventListener('change', (event) => {
     const fileList = event.target.files;
     //chamar o leitorArquivoXLSX
     leitor_arquivoXLSX = new LeitorArquivoXLSX(fileList[0]);
-    leitor_arquivoXLSX.parseExcel().then( resolve=> console.log( resolve))
+    leitor_arquivoXLSX.xlsxToJson().then(()=> {
+        //dados do arquivo excel
 
+        let data = leitor_arquivoXLSX.data; //fazer isso ser um for
+        //chamar JsonToView -- jogar os dados JSON na tabela
+        data.forEach( d=>{
+            //cada planilha do arquivo Excel assume uma posição do array data
+            toView = new JsonToView(d,"tableView");
+        })
+    })
 });
-
 
 
 
---
+++ js/table/JsonToView.js
@@ -0,0 +1,43 @@
+class JsonToView{
+    //recebe um JSON, converte para array e joga para uma tabela
+    //recebe a div que eu vou por os dados - vocabularioView
+
+    constructor(dataJson, idView) {
+        this.data = dataJson;
+        this.idView = idView;
+        this.toView();
+    }
+
+    toView = function(){
+        this.data = JSON.parse(this.data); //json to array
+        //console.log(this.data);
+        let countRow = document.getElementById(this.idView).rows.length;
+        for( let d in this.data){
+           this.appendRow(this.data[d], countRow);
+           countRow++;
+       }
+    }
+
+    appendRow(row, id){
+        let rowTable = "<tr>"+
+                "<td>"+id+"</td>"+
+                "<td>"+row["Palavras "]+"</td>"+
+                "<td>"+row["Significado"]+"</td>"+
+                "<td>"+row["Pronuncia GG"]+"</td>"+
+                "<td>"+this.formatDate(row["Data aprendizado"])+"</td>"+
+                "<td> <button onclick=\"\" type=\"button\" class=\"btn btn-outline-secondary\" ><i class=\"bi bi-trash\"></i></button></td>"+
+                "<td>  <button onclick=\"\" type=\"button\" class=\"btn btn-outline-secondary\" ><i class=\"bi bi-pencil\"></i> </button></td>"+
+              "</tr>";
+        //$(this.idView).find('tbody').append(rowTable);
+        $("#vocabularioView tr:last").after(rowTable);
+    }
+
+    formatDate(dateIn){
+        let date = new Date(dateIn);
+        let formattedDate= ((date.getDate() )) + "/" + ((date.getMonth() + 1)) + "/" + date.getFullYear();
+        return formattedDate;
+        //return date;
+    }
+
+
+}
