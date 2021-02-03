77739934c5e0d8b1d7178d875995994e51bfe4d3
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-03 10:24:10 -0300

    método salvar arquivo XLSX
    renomei a classe para fazer mais sentido, pois ela não somente lê um arquivo mas também escreve

--- js/arquivo/Excel/binario/FileXLSX.js
+++ js/arquivo/Excel/binario/FileXLSX.js
@@ -1,13 +1,12 @@
-class LeitorArquivoXLSX{
+class FileXLSX{
 
     //no momento só aceita um único arquivo
     constructor(file){
-        this.file=file;
-        this.idTable = "vocabularioView";
+        this.idTable = "tableView";
         this.data;
     }
 
-    async parseExcel() {
+    async parseExcel(file) {
         return new Promise((resolve, reject) => {
             var reader = new FileReader();
             reader.onload = function (e) {
@@ -32,26 +31,34 @@ class LeitorArquivoXLSX{
                 reader.abort();
                 reject(console.warn(ex));
             };
-
-            reader.readAsBinaryString(this.file)
+            reader.readAsBinaryString(file)
         });
            
     };
 
 
 
-    async xlsxToJson(){
+    async xlsxToJson(file){
         try {
-            const fileContents = await this.parseExcel();
+            const fileContents = await this.parseExcel(file);
             this.data = fileContents;
         } catch (e) {
             console.warn(e.message)
-        }finally {
-            this.imprimeData();
         }
     }
-
-    imprimeData(){
-        jQuery('#xlx_json').val("fileContents: "+this.data);
+    saveFileAux(s){
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i<s.length; i++) {
+            view[i] = s.charCodeAt(i) & 0xFF;
+            if (i ===0 )console.log(view[i])
+        }
+        return buf;
+    }
+    saveFile(){
+        var wb = XLSX.utils.table_to_book(document.getElementById(this.idTable), {sheet:"Meu vocabulário", raw: true} );
+        //console.log(wb["Data aprendizado"].v); //  default v value '4.56'
+        var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+        saveAs(new Blob([this.saveFileAux(wbout)],{type:"application/octet-stream"}), 'VocabularioAux.xlsx');
     }
 }
